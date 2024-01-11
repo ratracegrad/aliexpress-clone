7b0971f4ad3c16f9a0719d3f4b24edb6342c4f25
Jennifer Bland <ratracegrad@gmail.com>
2024-01-11 13:01:10 -0500

    handle isLoading

--- app.vue
+++ app.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
+import { useUserStore } from '@/stores/user'
 
 useHead({
   title: appName,
+})
+
+const userStore = useUserStore()
+const route = useRoute()
+const windowWidth = ref(process.client ? window.innerWidth : 0)
+
+onMounted(() => {
+  userStore.isLoading = false
+  window.addEventListener('resize', () => {
+    windowWidth.value = window.innerWidth
+  })
+})
+
+watch(() => windowWidth.value, () => {
+  if (windowWidth.value >= 767)
+    userStore.isMenuOverlay = false
+})
+
+watch(() => route.fullPath, () => {
+  userStore.isLoading = false
 })
 </script>
 
@@ -10,6 +31,12 @@ useHead({
   <div class="h-full w-full bg-[#FAFAFA]">
     <NuxtLayout>
       <NuxtPage />
+      <MenuOverlay
+        :class="[
+          { 'max-h-full transition-all duration-200 ease-in-visible': userStore.isMenuOverlay },
+          { 'max-h-0 transition-all duration-200 ease-out-invisible': !userStore.isMenuOverlay },
+        ]"
+      />
     </NuxtLayout>
   </div>
 </template>
