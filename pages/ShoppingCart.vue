e3c2ed680c409825bfe1df913dab8547b7541a37
Jennifer Bland <ratracegrad@gmail.com>
2024-01-14 13:28:14 -0500

    updated to use data from database

--- pages/ShoppingCart.vue
+++ pages/ShoppingCart.vue
@@ -10,23 +10,8 @@ interface Product {
 }
 
 const userStore = useUserStore()
+const user = useSupabaseUser()
 const selectedArray = ref([]) as Ref<Product[]>
-const products = [
-  {
-    id: 4,
-    name: 'Product 4',
-    price: 4796,
-    url: 'https://picsum.photos/id/11/800/800',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna eget massa mollis aliquam. Donec euismod, nisl eget aliquet ultricies, nunc nisl aliquet nunc, quis ali',
-  },
-  {
-    id: 5,
-    name: 'Product 5',
-    price: 5995,
-    url: 'https://picsum.photos/id/12/800/800',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel urna eget massa mollis aliquam. Donec euismod, nisl eget aliquet ultricies, nunc nisl aliquet nunc, quis ali',
-  },
-]
 const cards = ref([
   'visa.png',
   'mastercard.png',
@@ -78,14 +63,14 @@ function goToCheckout() {
 
 <template>
   <div id="ShoppingCartPage" class="mx-auto mt-4 max-w-7xl px-2">
-    <div v-if="false" class="items.center h-[500px] flex justify-center">
+    <div v-if="!userStore.cart.length" class="items.center h-[500px] flex justify-center">
       <div class="pt-20">
         <img class="mx-auto" width="250" src="/cart-empty.png" alt="empty cart">
 
         <div class="mt-4 text-center text-xl">
           No items yet?
         </div>
-        <div v-if="true" class="flex text-center">
+        <div v-if="!user" class="flex text-center">
           <NuxtLink to="/auth" class="mt-4 w-full rounded-full bg-rose-500 p-1.5 text-xl text-white font-semibold">
             Sign in
           </NuxtLink>
@@ -97,7 +82,7 @@ function goToCheckout() {
       <div class="md:w-[65%]">
         <div class="rounded-lg bg-white p-4">
           <div class="mb-2 text-2xl font-bold">
-            Shopping Cart(0)
+            Shopping Cart({{ userStore.cart.length }})
           </div>
         </div>
 
@@ -108,7 +93,7 @@ function goToCheckout() {
         </div>
 
         <div id="Items" class="mt-4 rounded-lg bg-white p-4">
-          <div v-for="product in products" :key="product.id">
+          <div v-for="product in userStore.cart" :key="product.id">
             <CartItem :product="product" :selected-array="selectedArray" @selected-radio="selectedRadioFunc" />
           </div>
         </div>
